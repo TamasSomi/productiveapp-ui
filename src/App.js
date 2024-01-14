27cc6d684fb24e87df384ca61bac8f3ef5dd3a8b
TamasSomi <>
2024-01-14 10:31:30 +0000

    install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Button from "react-bootstrap/Button";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <h1>Hello, world!</h1>
-    
+        <Button variant="primary">Primary</Button>
     </div>
   );
 }
