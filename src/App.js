ff6d52958c73c9325364917c2a5d8e8c9db4c765
TamasSomi <>
2024-01-14 11:57:24 +0000

    add favicon, logo basic navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Button from "react-bootstrap/Button";
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello, world!</h1>
-        <Button variant="primary">Primary</Button>
+        <NavBar />
     </div>
   );
 }
