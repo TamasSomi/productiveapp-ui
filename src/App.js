d295d81c4d5b40aeb8c393b52d087831e6f47f4e
TamasSomi <>
2024-01-15 18:24:39 +0000

    add currentUserContext and refactor using custom context hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
-import {Route,Switch} from 'react-router-dom';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
-
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
     <div className={styles.App}>
-        <NavBar />
-        <Container className={styles.Main}>
-            <Switch>
-                <Route exact path="/" render={()=> <h1>Home Page</h1> } />
-                <Route exact path="/signin" render={()=> <SignInForm /> } />
-                <Route exact path="/signup" render={()=> <SignUpForm /> } />
-                <Route render={()=><p>Page not found!</p>} />
-            </Switch>
-        </Container>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,60 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from '../assets/productive-logo.png';
-import styles from '../styles/NavBar.module.css'
+import logo from "../assets/productive-logo.png";
+import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
-    return (
-      <Navbar className={styles.NavBar} expand="md" fixed="top">
-        <Container>
-            <NavLink to="/">
+  const currentUser = useCurrentUser();
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
-          </Navbar.Brand></NavLink>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-start">
-              <NavLink  exact className={styles.NavLink} activeClassName={styles.Active} to="/">
-                <i className="fas fa-home"></i>Home
-              </NavLink>
-              <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
-                <i className="fas fa-sign-in-alt"></i>Sign in
-              </NavLink>
-              <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
-                <i className="fas fa-user-plus"></i>Sign up
-              </NavLink>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  };
-  
-  export default NavBar;
+          </Navbar.Brand>
+        </NavLink>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+
+            {currentUser ? loggedInIcons : loggedOutIcons}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
