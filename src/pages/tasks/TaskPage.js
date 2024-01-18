089d7de6d00545313f48f242d7948202976ac30f
TamasSomi <>
2024-01-18 16:00:39 +0000

    Display message to the user if not permitted to view the task

--- src/pages/tasks/TaskPage.js
+++ src/pages/tasks/TaskPage.js
@@ -1,21 +1,19 @@
 import React, { useEffect, useState } from "react";
-
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import Task from "./Task";
-import Note from "../notes/Note"
+import Note from "../notes/Note";
 import NoteCreateForm from "../notes/NoteCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
 
 function TaskPage() {
   const { id } = useParams();
   const [task, setTask] = useState({ results: [] });
-
   const currentUser = useCurrentUser();
   const profile_image = currentUser?.profile_image;
   const [notes, setNotes] = useState({ results: [] });
@@ -23,7 +21,7 @@ function TaskPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: task }, {data: notes}] = await Promise.all([
+        const [{ data: task }, { data: notes }] = await Promise.all([
           axiosReq.get(`/tasks/${id}`),
           axiosReq.get(`/notes/?task=${id}`)
         ]);
@@ -37,12 +35,32 @@ function TaskPage() {
     handleMount();
   }, [id]);
 
+  // Check if the user is logged in
+  const isLoggedIn = !!currentUser;
+
+  // Check if the user is the profile owner
+  const isProfileOwner = isLoggedIn && currentUser.profile_id === task.results[0]?.profile_id;
+
   return (
     <Row className="h-100">
       <Col className="mx-auto py-2 p-0 p-lg-2" lg={8}>
-        <Task {...task.results[0]} setTasks={setTask} taskPage />
-        <Container className={appStyles.Content}>
-          {currentUser ? (
+        {isLoggedIn ? (
+          isProfileOwner ? (
+            <Task {...task.results[0]} setTasks={setTask} taskPage />
+          ) : (
+            <span>You are not permitted to view this content.</span>
+          )
+        ) : (
+        <span>
+        Please{" "}
+        <Link to="/signin" className={appStyles.LoginLink}>
+          log in
+        </Link>{" "}
+        to view this content.
+      </span>
+        )}
+        {isProfileOwner && (
+          <Container className={appStyles.Content}>
             <NoteCreateForm
               profile_id={currentUser.profile_id}
               profileImage={profile_image}
@@ -50,21 +68,15 @@ function TaskPage() {
               setTask={setTask}
               setNotes={setNotes}
             />
-          ) : notes.results.length ? (
-            "Notes"
-          ) : null}
-          {notes.results.length ? (
-            notes.results.map(note => (
-                <Note key={note.id} {...note}
-                setTask={setTask}
-                setNotes={setNotes} />
-            ))
-          ) : currentUser ? (
-            <span>No notes have been added yet.</span>
-          ) : (
-            <span>No notes yet.</span>
-          )}
-        </Container>
+            {notes.results.length ? (
+              notes.results.map((note) => (
+                <Note key={note.id} {...note} setTask={setTask} setNotes={setNotes} />
+              ))
+            ) : (
+              <span>No notes have been added yet.</span>
+            )}
+          </Container>
+        )}
       </Col>
     </Row>
   );
--- src/pages/tasks/TasksPage.js
+++ src/pages/tasks/TasksPage.js
@@ -3,14 +3,13 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import Task from './Task';
+import Task from "./Task";
 import Asset from "../../components/Asset";
 import appStyles from "../../App.module.css";
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-
 
 function TasksPage({ message, filter = "" }) {
   const currentUser = useCurrentUser();
@@ -32,29 +31,29 @@ function TasksPage({ message, filter = "" }) {
     fetchTasks();
   }, [filter, pathname]);
 
-  return (
-    <Row className="h-100">
-      <Col className="mx-auto py-2 p-0 p-lg-2" lg={8}>
-        {tasks.loading ? (
-          <Container className={appStyles.Content}>
-            <Asset spinner />
-          </Container>
-        ) : (
-          <>
-            {tasks.results.length ? (
-              tasks.results.map((task) => (
-                <Task key={task.id} {...task} setTasks={setTasks} />
-              ))
-            ) : (
-              <Container className={appStyles.Content}>
-                <Asset src={NoResults} message={message} />
-              </Container>
-            )}
-          </>
-        )}
-      </Col>
-    </Row>
-  );
+    return (
+      <Row className="h-100">
+        <Col className="mx-auto py-2 p-0 p-lg-2" lg={8}>
+          {tasks.loading ? (
+            <Container className={appStyles.Content}>
+              <Asset spinner />
+            </Container>
+          ) : (
+            <>
+              {tasks.results.length ? (
+                tasks.results.map((task) => (
+                  <Task key={task.id} {...task} setTasks={setTasks} />
+                ))
+              ) : (
+                <Container className={appStyles.Content}>
+                  <Asset src={NoResults} message={message} />
+                </Container>
+              )}
+            </>
+          )}
+        </Col>
+      </Row>
+    );
 }
 
 export default TasksPage;
