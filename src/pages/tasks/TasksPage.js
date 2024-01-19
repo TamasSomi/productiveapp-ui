17dd83f0644848b81d1c2482243b8ff54f7e435b
TamasSomi <>
2024-01-19 12:57:25 +0000

    dispay sign in message on home/tasks page for unauthorised user

--- src/pages/tasks/TasksPage.js
+++ src/pages/tasks/TasksPage.js
@@ -6,31 +6,40 @@ import Container from "react-bootstrap/Container";
 import Task from "./Task";
 import Asset from "../../components/Asset";
 import appStyles from "../../App.module.css";
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function TasksPage({ message, filter = "" }) {
-  const currentUser = useCurrentUser();
-  const profile_id = currentUser?.profile_id || "";
-  const [tasks, setTasks] = useState({ results: [], loading: true });
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    const fetchTasks = async () => {
-      try {
-        const { data } = await axiosReq.get("/tasks/");
-        const userTasks = data.filter((task) => task.profile_id === profile_id);
-        setTasks({ results: userTasks, loading: false });
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    fetchTasks();
-  }, [filter, pathname]);
-
+    const currentUser = useCurrentUser();
+    const history = useHistory();
+    const profile_id = currentUser?.profile_id || "";
+    const [tasks, setTasks] = useState({ results: [], loading: true });
+    const { pathname } = useLocation();
+  
+    useEffect(() => {
+      const fetchTasks = async () => {
+        try {
+          const { data } = await axiosReq.get("/tasks/");
+          const userTasks = data.filter((task) => task.profile_id === profile_id);
+          setTasks({ results: userTasks, loading: false });
+        } catch (err) {
+          console.log(err);
+        }
+      };
+  
+      fetchTasks();
+    }, [filter, pathname, currentUser, history]);
+  
+    if (!currentUser) {
+      return (
+        <Container className={appStyles.Content}>
+          <p>Please <a href="/signin">sign in</a> to add a task. </p>
+        </Container>
+      );
+    }
+  
     return (
       <Row className="h-100">
         <Col className="mx-auto py-2 p-0 p-lg-2" lg={8}>
@@ -54,6 +63,6 @@ function TasksPage({ message, filter = "" }) {
         </Col>
       </Row>
     );
-}
-
-export default TasksPage;
+  }
+  
+  export default TasksPage;
